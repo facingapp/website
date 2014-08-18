4cadb8765458519b671f90fad361b6c1b8b8e3a1
Peter Schmalfeldt <manifestinteractive@gmail.com>
2014-08-18 19:52:13 -0400

    Removing more plugins from repo so they an be separate from project ( shell script will install them later ).

--- app/facing/platforms/ios/Facing/Plugins/nl.x-services.plugins.flashlight/Flashlight.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-
-@interface Flashlight : CDVPlugin
-
-- (void)available:(CDVInvokedUrlCommand*)command;
-- (void)switchOn:(CDVInvokedUrlCommand*)command;
-- (void)switchOff:(CDVInvokedUrlCommand*)command;
-
-@end
